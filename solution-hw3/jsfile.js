59781d40a763839ec2ec2e0055bf92f2fffb9904
jsklar03 <jonah.sklar@gmail.com>
2023-09-21 21:41:14 -0400

    Final with dynamic dropdowns

--- solution-hw3/jsfile.js
+++ solution-hw3/jsfile.js
@@ -1,6 +1,45 @@
 // I used this https://www.w3schools.com/js/js_htmldom_html.asp
+// let Glazing_Dict = {
+//     original: {name: "Keep Original", value: 0, str_value:"keep_original"},
+//     sugar: {name:"Sugar Milk", value: 0, str_value:"sugar_milk"},
+//     vanilla: {name:"Vanilla Milk", value: 0.50, str_value: "vanilla_milk"},
+//     chocolate: {name: "Double Chocolate", value: 1.50, str_value: "double_chocolate" }
+// }
+
+// let Pack_Size_Dict = {
+//     one: {name: "1", value: 1},
+//     three: {name: "3", value: 3},
+//     six: {name: "6", value: 5},
+//     twelve: {name: "12", value: 10}
+// }
 
 let base_price = 2.49;
+let glaze_name = ["Keep Original", "Sugar Milk", "Vanilla Milk", "Double Chocolate"];
+let glaze_price = [0, 0, 0.50, 1.50];
+let glaze_value = ["keep_original", "sugar_milk", "vanilla_milk", "double_chocolate"];
+let pack_size_name = ["1","3","6","12"];
+let pack_size_num = ["1", "3", "6", "12"];
+
+let glazing_option = document.querySelector("#glazing_dropdown");
+let pack_dropdown = document.querySelector("#pack_size_dropdown");
+
+glazing_option.addEventListener("load", populate_glaze_dropdown());
+
+function populate_glaze_dropdown(){
+    for (let i in glaze_name, glaze_value){
+        // console.log(glaze_name[i]);
+        let new_element = document.createElement("option");
+        new_element.innerHTML = glaze_name[i];
+        new_element.value = glaze_value[i];
+        glazing_option.appendChild(new_element);
+    }
+    for (let i in pack_size_name, pack_size_num){
+        let new_pack_el = document.createElement("option");
+        new_pack_el.innerHTML = pack_size_name[i];
+        new_pack_el.value = pack_size_num[i];
+        pack_dropdown.appendChild(new_pack_el);
+    }
+}
 
 // prices of rolls
 const rollPrice = {
@@ -30,68 +69,15 @@ function Price_Change(element){
     let glazing_option = document.getElementById("glazing_dropdown").value;
     console.log(glazing_option);
     let pack_option = document.getElementById("pack_size_dropdown").value;
-    console.log(pack_option)
+    console.log(pack_option);
     let glazing_price = glazingPrice[glazing_option];
     console.log(glazing_price);
     let pack_size = packSize[pack_option];
-    console.log(pack_size);
+    console.log(pack_size)
     new_price = (base_price + glazing_price) * pack_size;
-    let final_price = new_price.toFixed(2)
-    console.log(final_price)
+    let final_price = new_price.toFixed(2);
     document.getElementById("detail_price").innerText = "$"+ final_price;
 };
-
-// function GlazeChange(element){
-//     const Glazing_Change = element.value;
-//     if (Glazing_Change== "keep_original"){
-//         new_price = original_price + 0;
-//     }
-//     else if (Glazing_Change=="sugar_milk"){
-//         new_price = original_price + 0;
-//     }
-//     else if (Glazing_Change=="vanilla_milk"){
-//         let old_price = document.getElementById("detail_price").innerHTML = new_price;
-//         new_price = original_price + vanilla_milk_glaze;
-//         console.log(new_price);
-//         old_price = document.getElementById("detail_price").innerHTML = new_price;
-//         let vanilla_milk_glaze_price = new_price;
-//     }
-//     else if (Glazing_Change=="double_chocolate"){
-//         let old_price = document.getElementById("detail_price").value;
-//         new_price = original_price + double_choc_glaze;
-//         console.log(new_price);
-//         old_price = document.getElementById("detail_price").innerHTML = new_price;
-//         let double_choc_base_price = new_price;
-//     }
-
-// }
-
-// function Pack_Size_Price_Adaptation(element){
-//     let Pack_Size_Change = element.value;
-//     Pack_Size_Change = parseFloat(Pack_Size_Change);
-//     if (Pack_Size_Change == 1){
-//         new_price = new_price;
-//         console.log(new_price);
-//     }
-//     else if(Pack_Size_Change == 3){
-//         let old_price = document.getElementById("detail_price").value;
-//         new_price = new_price * 3;
-//         console.log(new_price);
-//         old_price = document.getElementById("detail_price").innerHTML= new_price;
-//     }
-//     else if(Pack_Size_Change == 6){
-//         let old_price = document.getElementById("detail_price").value;
-//         new_price = new_price * 5;
-//         console.log(new_price);
-//         old_price = document.getElementById("detail_price").innerHTML= new_price;
-//     }
-//     else if(Pack_Size_Change == 12){
-//         let old_price = document.getElementById("detail_price").value;
-//         new_price = new_price * 10;
-//         console.log(new_price);
-//         old_price = document.getElementById("detail_price").innerHTML= new_price;
-//     }
-// }
 
 
 
