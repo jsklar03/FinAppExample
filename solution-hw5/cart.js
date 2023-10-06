0ee9c828d7bd74d6d7cba5639bf71d64a7c29b7c
jsklar03 <jonah.sklar@gmail.com>
2023-10-06 19:14:00 -0400

    need to add remove functionality and css

--- solution-hw5/cart.js
+++ solution-hw5/cart.js
@@ -7,57 +7,122 @@ Once cart page is dynamically populating HTML, then add the remove functionality
 update the cart's total price.
 */
 
-
-//----- For later ----------
-function add_to_cart (rollType,rollGlaze,packSize,basePrice){
-    let new_cart_item = new Roll(rollType, rollGlaze, packSize, basePrice);
-    new_cart_item = rollType;
-    cart.push(new_cart_item);
-    cartSet.add(new_cart_item);
+const glazing_dict = {
+    "Keep Original": 0,
+    "Sugar Milk": 0,
+    "Vanilla Milk": 0.50,
+    "Double Chocolate": 1.50
 }
 
-const cart_item_1 = add_to_cart(
+const pack_size_dict = {
+    "1":1,
+    "3":3,
+    "6":5,
+    "12":10
+}
+
+//----- For later ----------
+class Roll {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;
+    }
+}
+
+cartArray = [];
+
+const cart_item_1 = new Roll(
     "Original",
     "Sugar Milk",
     "1",
     2.49
 )
 
-const cart_item_2 = add_to_cart(
+const cart_item_2 = new Roll(
     "Walnut",
     "Vanilla Milk",
     "12",
-    39.90
+    3.49
 )
 
-const cart_item_3 = add_to_cart(
-    "Apple",
+const cart_item_3 = new Roll(
+    "Raisin",
     "Sugar Milk",
     "3",
-    8.97
+    2.99
 )
 
-const cart_item_4 = add_to_cart(
+const cart_item_4 = new Roll(
     "Apple",
-    "Original",
+    "Keep Original",
     "3",
-    10.47
+    3.49
 )
 
-const template = document.querySelector("#cart_template");
-const clone = template.content.cloneNode(true);
-clone.querySelector("element");
-clone.querySelector(".cart_image");
+cartArray.push(cart_item_1, cart_item_2, cart_item_3, cart_item_4);
+console.log(cartArray);
 
-for (let item in cartSet){
-
+for (let i of cartArray){
+    i.calculated_price = (i.basePrice + glazing_dict[i.glazing]) * pack_size_dict[i.size]
+    createElement(i);
 }
 
+console.log(cartArray)
 
-// let Remove = document.getElementsByClassName("remove");
-// Remove.addEventListener("click", Remove_Item)
+function createElement(roll) {
+    // make a clone of the notecard template
+    const template = document.querySelector('#cart_template');
+    const clone = template.content.cloneNode(true);
+    
+    // connect this clone to our notecard.element
+    // from this point we only need to refer to notecard.element
+    roll.element = clone.querySelector('.shopping_cart_item');
+  
+    const cartListElement = document.querySelector('.shopping_cart');
+    cartListElement.appendChild(roll.element);
 
-// function Remove_Item(){
+    updateRoll(roll)
+}
 
-// }
+function updateRoll(roll){
+    const roll_img = roll.element.querySelector(".cart_image");
+    roll_img.src = '../assets/products/'+ rolls[roll.type].imageFile;
+
+    const roll_title = roll.element.querySelector(".cart_title");
+    roll_title.innerText = roll.type + " Cinnamon Roll";
+
+    const glaze_title = roll.element.querySelector(".cart_glazing");
+    glaze_title.innerText = "Glazing: " + roll.glazing;
+
+    const pack_size_title = roll.element.querySelector(".cart_size");
+    pack_size_title.innerText = "Pack Size: " + roll.size;
+
+    const item_price = roll.element.querySelector(".cart_item_price");
+    item_price.innerText = "$" + roll.calculated_price.toFixed(2);
+
+    updateTotalPrice(roll);
+}
+
+function updateTotalPrice(roll){
+    let total_price = 0.00;
+    for (let i of cartArray){
+        console.log(i)
+        let total_price_el = document.querySelector(".total_price");
+        console.log(total_price_el)
+        total_price = total_price + i.calculated_price;
+        console.log(total_price)
+        let final_price = total_price.toFixed(2);
+        total_price_el.innerText= "$" + final_price;
+    }
+}
+
+const remove_button = document.getElementsByClassName("remove");
+remove_button.addEventListener("click",remove_item);
+
+function remove_item(){
+    console.log("button")
+}
+
 
